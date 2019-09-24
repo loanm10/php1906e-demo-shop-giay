b3fbca4ba48a8f2ce7d044788d637bffea55f7b6
loanm10 <52204715+loanm10@users.noreply.github.com>
2019-09-24 19:45:26 +0700

    asdasd

---
+++ resources/views/admin/category/add_cate.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layout.master')
+@section('content')
+    <form action="" method="post">
+        <table border="1">
+            <tr>
+                <td>Category name</td>
+                <td><input name="name"></td>
+            </tr>
+            <tr>
+                <td>order_display</td>
+                <td><input name="order_display"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="submit" name="save">Save</button>
+                    <button type="submit" name="cancel">Cancel</button>
+                </td>
+            </tr>
+        </table>
+        {{csrf_field()}}
+    </form>
+@endsection
